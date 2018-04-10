57be248d9df4b04558b8192bbe271253ade2f857
Samir <sagayev@gmail.com>
2018-04-10 12:24:34 +0400

    changing the behaviour of a function

--- decor.py
+++ decor.py
@@ -39,4 +39,21 @@ def parent(num):
     
 foo=parent(10)
 bar=parent(11)
+
+def my_decorator(some_function):
+    
+    def wrapper():
+        print("Something is happening before some_function() is called")
+        
+        some_function()
+        
+        print("Something is happening after some_function is called")
+        
+    return wrapper
+    
+def just_some_function():
+    print("Wheee!")
+    
+just_some_function = my_decorator(just_some_function)
+just_some_function()
         

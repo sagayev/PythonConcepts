b822b6909d1c5c6bbd9dfb031ddd6cda59da83e7
Samir <sagayev@gmail.com>
2018-04-10 12:07:41 +0400

    cannot call child functions outside of function

--- decor.py
+++ decor.py
@@ -22,3 +22,15 @@ print(call_foo_with_arg(foo, 3))
 
 ##Nested Functions
 
+def parent():
+    print("Printing from the parent() function.")
+    
+    def first_child():
+        return "Printing from the first_child() function." 
+    
+    def second_child():
+        return "Printing from the second_child() function."
+        
+    print(first_child())
+    print(second_child())
+    
